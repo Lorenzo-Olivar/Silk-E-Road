de6def01815bca512c30a96618e4a1adfedf176c
Zo Olivar <lorenzoolivar9@gmail.com>
2023-11-08 01:28:54 -0500

    finished making the models and api routes for the models

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    // find all products
+    const productData = await Product.findAll({
+      include: [{model: Category}, {model: ProductTag}, {model: Tag}]
+    });
+    res.status(200).json(productData);
+    // be sure to include its associated Category and Tag data
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    // find a single product by its `id`
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: ProductTag}, {model: Tag}]
+    });
+    if (!productData) {
+      res.status(404).json('No Product by this id!')
+    };
+    res.status(200).json(productData);
+    // be sure to include its associated Category and Tag data
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
@@ -92,7 +111,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(404).json('No Product by this id!')
+    };
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
   // delete one product by its `id` value
 });
 
